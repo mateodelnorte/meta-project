1f4441e3e5f94337cb74a4e83cb21d3b519f402d
Patrick Scott <pat@patscott.io>
2019-06-06 21:33:06 -0400

    feat: refactor add to import/create
    BREAKING CHANGE: deprecated add for more explicit import/create

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 module.exports.register = program => {
-  program.command('project', 'add & remove child repositories');
+  program.command('project', 'create, import, and remove child repositories');
 };
