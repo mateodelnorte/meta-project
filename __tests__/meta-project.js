3190edea00e5c209f67a185f98922f769a6fe61e
Patrick Scott <pat@patscott.io>
2019-06-11 13:29:24 -0400

    chore: index

--- __tests__/meta-project.js
+++
@@ -1,9 +0,0 @@
-const metaProject = require('meta-project');
-
-jest.spyOn(process, 'exit').mockImplementation(() => {});
-
-describe('bin/meta-project', () => {
-  it('should exist', () => {
-    expect(metaProject).toBeDefined();
-  });
-});
