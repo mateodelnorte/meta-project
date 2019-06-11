1bccd018ae83cd22ad537220d188b3f1473f7839
Patrick Scott <pat@patscott.io>
2019-06-11 13:25:08 -0400

    chore: spy on process.exit to change impl

--- __tests__/meta-project.js
+++ __tests__/meta-project.js
@@ -1,5 +1,7 @@
 const metaProject = require('meta-project');
 
+jest.spyOn(process, 'exit').mockImplementation(() => {});
+
 describe('bin/meta-project', () => {
   it('should exist', () => {
     expect(metaProject).toBeDefined();
